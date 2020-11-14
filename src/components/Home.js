bb5900fd2c2beb47dae5f83d31f416850d3efce7
Xiaoyi Chen <chenxy@yjhealth.cn>
2020-11-14 05:57:03 -0800

    update search

--- src/components/Home.js
+++ src/components/Home.js
@@ -76,6 +76,9 @@ const data = [
 const Home = () => {
 	const [activeTab, setActiveTab] = useState('search');
 	const [selectedMedicine, setSelectedMedicine] = useState();
+	const [searchBy, setSearchBy] = useState('name');
+	const [companyInfo, setCompanyInfo] = useState();
+	
 	// TODO: Please set the initial state to [] and use setSelectedMedicine to update the 
 	//       state value when the drug in the detail page changes
 	const [similarDrugs, setSimilarDrugs] = useState([
@@ -86,11 +89,24 @@ const Home = () => {
 
 	const onSearch = (value) => {
 		console.log('searching', value);
+		const fetchData = async () => {
+			if (searchBy == 'company') {
+					const response = await fetch("http://localhost:3500/api/get_drug_by_company", {
+						method: 'POST',
+						json: true,
+						body: {'company': value}
+					  }).then(res => res.json())
+					setCompanyInfo(response.company);
+					console.log(companyInfo);
+			}
+		}
+		fetchData();
 		// TODO: Add search functionality here
 	}
 
 	const onChangeRadio = (e) => {
 		console.log(`radio checked:${e.target.value}`);
+		setSearchBy(e.target.value);
 		// TODO: Integrate this with search, you can use state to store the seleted
 		//       radio button value, for usage please refer to line 77 & 78
 	}
@@ -129,6 +145,7 @@ return(
 				  <Radio.Button value="name">Name</Radio.Button>
 				  <Radio.Button value="sympton">Sympton</Radio.Button>
 				  <Radio.Button value="company">Company</Radio.Button>
+				  <Radio.Button value="drugclass">Drug class</Radio.Button>
 			  </Radio.Group>
 			  </div>
 			  <div className='home-search'>
@@ -140,6 +157,14 @@ return(
 				  onSearch={onSearch}
 				/>
 			  </div>
+			  {companyInfo &&
+					<div className='company-info-container'>
+						<div>Company: {companyInfo.address}</div>
+						<div>Founded: {companyInfo.founded}</div>
+						<div>link: {companyInfo.link}</div>
+						<div>StockPrice: {companyInfo.traded}</div>
+					</div>
+				}
 			  <div className='home-search-result'>
 				<SearchResult />
 			  </div>
@@ -155,12 +180,12 @@ return(
 					  <div className='similar-drug-wrapper'>
 						<Row gutter={16}>
 						{similarDrugs.map(drug => (
-					        <Col span={8}>
+							<Col span={8}>
 							<Card size="small" title={drug.name} bordered={false}>
 								description here
 							</Card>
 						  </Col>
-					    ))}
+						))}
 						</Row>
 					  </div>
 				  </div>
